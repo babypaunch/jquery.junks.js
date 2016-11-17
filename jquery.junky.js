760303982c4adfaab18e4b7f4de609bf98971770
babypaunch <babypaunch@gmail.com>
2016-11-17 18:12:33 +0900

    함수 수정 & 추가
    1. zIndex()
    2. String.jsonize()
    3. serialize() 추가

--- jquery.junky.js
+++ jquery.junky.js
@@ -30,25 +30,42 @@ $(document).on("keydown", function(e){
 });
 
 /*
+* 문서 내에서 z-index가 가장 높은 값 + 1을 반환
+*/
+var zIndex = function(){
+	var MAX = 2147483647;
+	var result = 0;
+
+	$("*").each(function(){
+		var zIndex = $(this).css("z-index") === "auto" ? 1 : parseInt($(this).css('z-index'));
+		if(zIndex > result){
+			result = zIndex;
+		}
+	});
+
+	return MAX > result ? result + 1 : MAX;
+}; //end: var zIndex = function(){
+
+/*
 * 문자열의 첫 글자만 대문자로 변경한다.
 */
 String.prototype.cap = function(){
 	return this.charAt(0).toUpperCase() + this.slice(1);
-}
+} //end: String.prototype.cap = function(){
 
 /*
 * 문자열에 3자리마다 comma를 추가한다.
 */
 String.prototype.comma = function(){
 	return this.replace(/\B(?=(\d{3})+(?!\d))/g, ","); 
-}
+} //end: String.prototype.comma = function(){
 
 /*
 * 문자열에서 comma를 제거한다.
 */
 String.prototype.strip = function(){
 	return this.replace(/[^\d]+/g, "");
-}
+} //end: String.prototype.strip = function(){
 
 /*
 * 만 나이 14세를 체크함.
@@ -65,29 +82,37 @@ String.prototype.isCriminalMinor = function(){
 	var dd = Number(now.getDate())
 
 	return yyyy - year > 14 ? (yyyy - year === 14 ? (mm < month ? true : (mm === month ? (dd > date ? true : false) : false)) : false) : true;
-}
+} //end: String.prototype.isCriminalMinor = function(){
 
+/*
+* url/map 패턴의 문자열을 json형태로 parsing한다.
+* separator는 첫번째 구분자
+* decouple은 key와 value로 나눌 구분자
+* 첫번째 물음표, 모든 {(left brace), }(right brace) 기호는 제거된다.
+*/
+String.prototype.jsonize = function(separator, decouple){
+	var json = {};
+
+	var arr = $.trim(this).replace(/^[\?]/, "").replace(/{/gi, "").replace(/}/gi, "").split(separator || ",");
+	for(var i = 0; i < arr.length; i++){
+		var pair = arr[i].split(decouple || "=");
+		if(pair.length === 2 && $.trim(pair[0]) !== ""){
+			json[$.trim(pair[0])] = $.trim(pair[1]);
+		}
+	}
+
+	return json;
+} //end: String.prototype.jsonize = function(separator, decouple){
+
+/*
+* jquery를 사용할 때, Object를 prototype을 이용해서 개발할 경우 에러가 발생하므로
+* json과 같이 Object를 다룰때는 별도의 객체에 담아서 아래와 같이 처리해야한다.
+*/
 var $J = {
-	/*
-	* 문서 내에서 z-index가 가장 높은 값 + 1을 반환
-	*/
-	lastIndex: function(){
-		var result = 0;
-
-		$("*").each(function(){
-			var zIndex = $(this).css("z-index") === "auto" ? 1 : parseInt($(this).css('z-index'));
-			if(zIndex > result){
-				result = zIndex;
-			}
-		});
-
-		return result + 1;
-	} //end: lastIndex: function(){
-
 	/*
 	* 배열내 json객체를 지정된 key명으로 정/역방향 정렬한다.
 	*/
-	, sort: function(arr, key, order){
+	sort: function(arr, key, order){
 		return arr.sort(function(a, b){
 			return ["desc", "dsc"].indexOf(order) === -1 ? (a[key] < b[key] ? - 1 : a[key] > b[key] ? 1 : 0) : (a[key] > b[key] ? - 1 : a[key] < b[key] ? 1 : 0);
 		});
@@ -106,22 +131,38 @@ var $J = {
 	} //end: , excepts: function(json, arr){
 
 	/*
-	* url/map 패턴의 문자열을 json형태로 parsing한다.
-	* separator는 첫번째 구분자
-	* decouple은 key와 value로 나눌 구분자
-	* 첫번째 물음표, 모든 {(left brace), }(right brace) 기호는 제거된다.
+	* 입력받은 json객체를 직렬화한다.
+	* options는 separator(string), couple(string), excepts(array), empty(boolean)를 가진다.
+	* json만 넘길경우엔 jquery의 직렬화와는 조금 다른 결과가 나온다. jquery의 serialize() 경우엔 한글이 인코딩된다.
+	* separator는 전체 구분자로 사용한다.
+	* couple은 key와 value로 묶을 구분자로 사용한다.
+	* excepts는 json 내에서 제외할 key를 배열로 입력해서 제외 처리한다.
+	* empty는 value가 공백인 값을 허용할지 결정한다. true는 허용, false는 제외
 	*/
-	, jsonize: function(obj, separator, decouple){
-		var json = {};
+	, serialize: function(json, options){
+		var str = "";
+		var defaults = {
+			separator: "&"
+			, couple: "="
+			, excepts: []
+			, empty: true
+		};
+		$.extend(defaults, options);
 
-		var arr = $.trim(obj).replace(/^[\?]/, "").replace(/{/gi, "").replace(/}/gi, "").split(separator || ",");
-		for(var i = 0; i < arr.length; i++){
-			var pair = arr[i].split(decouple || "=");
-			if(pair.length === 2 && $.trim(pair[0]) !== ""){
-				json[$.trim(pair[0])] = $.trim(pair[1]);
-			}
+		if(defaults.excepts.length > 0){
+			json = this.excepts(json, defaults.excepts);
 		}
 
-		return json;
-	} //end: , jsonize: function(obj, separator, decouple){
+		for(var keys = Object.keys(json), i = 0; i < keys.length; i++){
+			var key = keys[i];
+
+			if(!defaults.empty && json[key] === ""){
+				continue;
+			}
+
+			str += key + defaults.couple + json[key] + (i + 1 === keys.length ? "" : defaults.separator));
+		}
+
+		return str;
+	} //end: , serialize: function(json, options){
 }; //end: var $J = {
